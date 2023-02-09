8a91739e219ae4bdd6f6b490642ff030e14d91c2
Bigali <104558475+Pandarry@users.noreply.github.com>
2023-02-09 22:41:35 +0600

    1.8

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
 import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classNames/classNames'
 
 export default function App() {
     const { theme, toggleTheme } = useTheme()
 
     return (
-        <div className={`App ${theme}`}>
+        <div className={classNames('App', {}, [theme])}>
             <button onClick={toggleTheme}>Change theme</button>
             <Link to="/">Main link</Link>
             <Link to="/about">About link</Link>
