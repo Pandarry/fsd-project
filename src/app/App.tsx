bb2d4f240ef8652600d056e1250791fbeb207da0
Bigali <104558475+Pandarry@users.noreply.github.com>
2023-02-11 21:28:57 +0600

    2.3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import './styles/index.scss'
-import { Link } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from './providers/router'
 import { NavBar } from 'widgets/NavBar'
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 
 export default function App() {
-    const { theme, toggleTheme } = useTheme()
+    const { theme } = useTheme()
 
     return (
         <div className={classNames('App', {}, [theme])}>
             <NavBar />
             <AppRouter />
-            <button onClick={toggleTheme}>Change theme</button>
         </div>
     )
 }
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
 import styles from './NavBar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 
 interface NavBarProps {
     className?: string
@@ -10,6 +11,7 @@ interface NavBarProps {
 export const NavBar = ({ className }: NavBarProps) => {
     return (
         <div className={classNames(styles.NavBar)}>
+            <ThemeSwitcher />
             <div className={styles.Links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to="/">
                     Main link
