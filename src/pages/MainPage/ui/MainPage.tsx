90f4375f49fb45715c8001f750faff186fb79816
Bigali <104558475+Pandarry@users.noreply.github.com>
2023-02-20 17:12:32 +0600

    3.1

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,12 @@
+import { BugButton } from 'app/providers/ErrorBoundary'
 import { useTranslation } from 'react-i18next'
 
 export function MainPage() {
     const { t } = useTranslation()
-    return <div>{t('Главная')}</div>
+    return (
+        <div>
+            <BugButton />
+            {t('Главная')}
+        </div>
+    )
 }
